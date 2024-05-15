e9499c5295bc1036097f29ce0baf78e878bab1ae
yashaswikukretii <144528153+yashaswikukretii@users.noreply.github.com>
2024-05-16 01:08:04 +0530

    Revert "Update Program.cs"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, blah bjdednjdkjdkjeiuwjdhjjjajkjhjkhjkbjabdnqwnbdqwkjkhhahdjkhasjkdhkjashdjkhskjdhkbdlah");
+Console.WriteLine("Hello, blah bjdednjdkjdkjebdlah");
